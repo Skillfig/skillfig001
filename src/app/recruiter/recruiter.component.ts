2a9654843c10d6ec20c570659012294ea96fe087
SkillFig <skillfig@gmail.com>
2018-06-05 14:38:49 +0530

    update

--- src/app/recruiter/recruiter.component.ts
+++ src/app/recruiter/recruiter.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-recruiter',
   templateUrl: './recruiter.component.html',
-  styleUrls: ['./recruiter.component.less']
+  styleUrls: ['./recruiter.component.less'],
+  animations : [
+    trigger('goals', [
+      transition('* => *',[
+        query(':enter', style({opacity:0 }), {optional:true}),
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity:0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity:.5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity:1, transform: 'translateY(0)', offset: 1}),
+          ])) ]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class RecruiterComponent implements OnInit {
 
